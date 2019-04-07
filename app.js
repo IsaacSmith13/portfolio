25275498aaeead7c3b829958bde159365f6e0510
Isaac Smith <isaac13smith@gmail.com>
2019-04-07 23:31:29 +0000

    added link to about me and created about me page

--- app.js
+++ app.js
@@ -3,10 +3,14 @@ const app = express();
 
 app.set("view engine", "ejs");
 
-app.get("/",function(req,res) {
+app.get("/", function(req, res) {
     res.render("index");
 });
 
-app.listen(process.env.PORT,process.env.IP,function() {
+app.get("/about", function(req, res) {
+    res.render("about");
+});
+
+app.listen(process.env.PORT, process.env.IP, function() {
     console.log("Server started");
 });
