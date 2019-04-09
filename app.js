e1ef7ada90377c9ee9ebcfad0166b1b6c1b86ffe
Isaac Smith <isaac13smith@gmail.com>
2019-04-09 06:58:15 +0000

    added active page functionality to navbar & created contact page

--- app.js
+++ app.js
@@ -5,13 +5,16 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 app.get("/", function(req, res) {
-    res.render("index");
+    res.render("index", { page: "index" });
 });
 
 app.get("/about", function(req, res) {
-    res.render("about");
+    res.render("about", { page: "about" });
 });
 
+app.get("/contact", function(req, res) {
+    res.render("contact", { page: "contact" });
+});
 app.listen(process.env.PORT, process.env.IP, function() {
     console.log("Server started");
 });
